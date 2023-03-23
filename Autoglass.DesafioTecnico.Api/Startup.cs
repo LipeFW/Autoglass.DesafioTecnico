041b043b70fbe877c6d00247b48c9d4754d3fa8c
Felipe Weber <felipeweber2002@hotmail.com>
2023-03-23 20:09:37 -0300

    Dependency injection

--- Autoglass.DesafioTecnico.Api/Startup.cs
+++ Autoglass.DesafioTecnico.Api/Startup.cs
@@ -1,14 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Autoglass.DesafioTecnico.Infrastructure;
 
 namespace Autoglass.DesafioTecnico.Api
 {
@@ -25,6 +20,7 @@ namespace Autoglass.DesafioTecnico.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.ConfigureInfrastructure(this.Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
